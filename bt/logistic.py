0c7d4fdb323841b18f440164d2c5c6ab36e6639f
tks1998 <trankimsen1681998@gmail.com>
2019-05-18 19:15:19 +0700

    add model logistic

---
+++ bt/logistic.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from numba import jit
+from scipy.sparse import hstack
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes  import BernoulliNB
+from prepare import *
+"""
+    read data with pandas
+"""
+data = pd.read_json('Sarcasm_Headlines_Dataset.json', lines=True)
+
+""" get train set  """
+trainData = data[:20000]
+trainX = trainData['headline']
+trainY = trainData['is_sarcastic']
+""" get test set """ 
+
+testData = data[20000:]
+testX = testData['headline']
+testY = testData['is_sarcastic']
+#prepare data with tf idf 
+vectorizer = TfidfVectorizer().fit(trainX)
+vectorX = vectorizer.transform(trainX)
+vectortestX = vectorizer.transform(testX)
+@jit
+def sigmoid(S):
+    return 1/(1 + np.exp(-S))
+
+@jit
+def loss(p, y):
+    return np.sum(-y * np.log(p) - (1 - y) * np.log(1 - p))
+
+@jit
+# add 1 columum
+def add1Col(x):
+    intercept = np.ones((x.shape[0], 1))
+    return hstack((intercept, x))
+
+# def batch gradient descent
+@jit
+def BGD(x, y,alpha ,numIter=1000):
+    x = add1Col(x)
+    theta = np.zeros(x.shape[1])
+    m = x.shape[0]
+    for i in range(numIter):
+        z = x.dot(theta)
+        h = sigmoid(z)
+        theta = theta - (alpha / m) * x.transpose().dot((h - y))
+       
+    return theta
+alpha = 5
+theta = BGD(vectorX,trainY,alpha)
+vectortestX = add1Col(vectortestX)
+vectortestXX = vectortestX.tocsr()
+# p save label 
+# calculate label with sigmoid 
+p=[]
+
+for i in range(0,vectortestX.shape[0]):
+    if  sigmoid(vectortestXX[i].dot(theta))>=0.5:
+        p.append(1)
+    else:
+        p.append(0)
+output('logistic  ' , score(p,testY))
--- bt/process.py
+++ bt/process.py
@@ -8,7 +8,7 @@ from scipy.sparse import hstack
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes  import BernoulliNB
-from prepare import *
+
 """
     read data with pandas
 """
@@ -20,19 +20,23 @@ trainX = trainData['headline']
 trainY = trainData['is_sarcastic']
 
 
+
+
 """ get test set """ 
+
 testData = data[20000:]
 testX = testData['headline']
 testY = testData['is_sarcastic']
+
 
 """
     process data with module TfidfVectorizer
     Tfidf :Convert a collection of raw documents to a matrix of TF-IDF features.
 """
+
 vectorizer = TfidfVectorizer().fit(trainX)
 vectorX = vectorizer.transform(trainX)
 vectortestX = vectorizer.transform(testX)
-
 
 
 """
@@ -45,32 +49,28 @@ vectortestX = vectorizer.transform(testX)
     process knn with KNeighborsClassifier    
     choose number loop training 
 """
+"""
 number_point = 7 
 KNNmodel = KNeighborsClassifier(n_neighbors = number_point, metric='euclidean').fit(vectorX, trainY)
 y_traning  = KNNmodel.predict(vectortestX)
 output('KNN model: ' , score(y_traning,testY))
-
+"""
 """
     processing data with id3
     model sklearn support DecisionTreeClassifier process data with desicion tree 
 """
-
+"""
 desicion_tree = DecisionTreeClassifier().fit(vectorX, trainY)
 y_traning = desicion_tree.predict(vectortestX)
 output('Decision Tree model: ',score(y_traning,testY))
-
+"""
 """
     naive bayes model
 """
-
+"""
 naive_bayes = BernoulliNB().fit(vectorX, trainY)
 y_training= naive_bayes.predict(vectortestX)
 output('Naive Bayes model: ', score(testY, y_traning))
-
-
 """
-    code tay ~~ 
-"""
-
 
 
